f9f8e71f22b5902d69a0f122944f684758b6bb95
João Pedro Marques Chaves <jchaves@digitalhouse.com>
2025-08-18 12:05:48 -0300

    chore: prod env file fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import { environment } from './app/environments/environment';
+import { environment } from './environments/environment';
 import { enableProdMode } from '@angular/core';
 
 if (environment.production) {
