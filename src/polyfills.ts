18d6cf2a42024a5fde6f35a2984fa29b2dbdc524
João Pedro Marques Chaves <jchaves@digitalhouse.com>
2025-08-18 13:20:46 -0300

    update: angular 16 to angular 17

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -21,7 +21,7 @@
 /**
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js'; // ✅ NEW (works with Angular 17+)
 
 
 /***************************************************************************************************
